15a2aa81fee0a49423117d63600601411ec46138
timur <akimbaev.tim2002@gmail.com>
2023-11-29 22:25:00 -0800

    initial commit

--- swift-test/ContentView.swift
+++ swift-test/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, world, This is my first line code in swift app")
         }
         .padding()
     }
