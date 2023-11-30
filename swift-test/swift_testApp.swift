14e1ff26020ff5a3485d9baa923d9580c1697238
timur <akimbaev.tim2002@gmail.com>
2023-11-29 22:20:01 -0800

    Initial Commit

---
+++ swift-test/swift_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_testApp.swift
+//  swift-test
+//
+//  Created by Timur on 29.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct swift_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
